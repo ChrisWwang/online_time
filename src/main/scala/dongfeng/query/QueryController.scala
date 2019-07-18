c43d2bcf06d4dd02f85ac26be7fe4d70a5868b31
ChrisWwang <hi_wch@outlook.com>
2019-07-18 16:48:09 +0800

    7.18日 补充注释

--- src/main/scala/dongfeng/query/QueryController.scala
+++ src/main/scala/dongfeng/query/QueryController.scala
@@ -12,26 +12,31 @@ import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 object QueryController {
 
   def main(args: Array[String]): Unit = {
+    //  日志等级控制
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
 
+    //  获取sparkConf和sparkSession
     val sparkConf = SparkEngine.sparkConf2()
     val sparkSession = SparkEngine.session(sparkConf)
 
 
-    //--------------------------获取mongdb数据库的driver_online_record表-------------------
+    //--------------------获取mongodb数据库的driver_online_record表---------------------
     val driver_online_record: DataFrame = MongoSpark.load(sparkSession)
     driver_online_record.createOrReplaceTempView("driver_online_record")
-//        val uri: String = "mongodb://test:test@47.99.187.146:27017/och_test.reservation_order_notified_record_201907"
-//        val pushOrderTb = session.read.format("com.mongodb.spark.sql").options(
-//          Map("spark.mongodb.input.uri" -> uri,
-//            "spark.mongodb.input.partitioner" -> "MongoPaginateBySizePartitioner",
-//            //        "spark.mongodb.input.partitionerOptions.partitionKey"  -> "_id",
-//            "spark.mongodb.input.partitionerOptions.partitionSizeMB"-> "32"))
-//          .load()
-//        df.show()
 
-    //------------获取hbase数据库的driver_info表--------------
-    //加载司机表(driver_info)
+    //  另一种获取mongoDB数据的方式
+    //        val uri: String = "mongodb://test:test@47.99.187.146:27017/och_test.reservation_order_notified_record_201907"
+    //        val pushOrderTb = session.read.format("com.mongodb.spark.sql").options(
+    //          Map("spark.mongodb.input.uri" -> uri,
+    //            "spark.mongodb.input.partitioner" -> "MongoPaginateBySizePartitioner",
+    //            "spark.mongodb.input.partitionerOptions.partitionKey"  -> "_id",
+    //            "spark.mongodb.input.partitionerOptions.partitionSizeMB"-> "32"))
+    //          .load()
+    //        df.show()
+
+
+    //-----------------------获取hbase数据库的driver_info表--------------------
+    //  加载司机表(driver_info)
     var driver_info = sparkSession
       .read
       .format(GlobalConfigUtils.customHbasePath)
@@ -41,21 +46,20 @@ object QueryController {
           GlobalConfigUtils.hbase_table_name -> GlobalConfigUtils.table_driver_info,
           GlobalConfigUtils.hbase_table_schema -> GlobalConfigUtils.driverInfo_HbaseSchema
         )).load()
-//    val driver_info: DataFrame = readHbase.readHbase_driver_info(sparkConf , sparkSession)
-    //创建司机信息表
+    //  测试读取hbase表
+    //  val driver_info: DataFrame = readHbase.readHbase_driver_info(sparkConf , sparkSession)
+    //  创建司机信息表
     driver_info.createOrReplaceTempView("driver_info")
-    driver_info.show()
-    //本地测试读取mysql数据库的driver_info表
-//    val driver_info: DataFrame = sparkSession.read.format("jdbc")
-//      .option("url", "jdbc:mysql://47.111.68.2:3306/och_test?useUnicode=true&characterEncoding=utf8")
-//      .option("user", "root")
-//      .option("password", "123456")
-//      .option("dbtable", "driver_info")
-//      .option("driver", "com.mysql.jdbc.Driver")
-//      .load()
 
-
-
+    //  本地测试读取mysql数据库的driver_info表
+    //    val driver_info: DataFrame = sparkSession.read.format("jdbc")
+    //      .option("url", "jdbc:mysql://47.111.68.2:3306/och_test?useUnicode=true&characterEncoding=utf8")
+    //      .option("user", "root")
+    //      .option("password", "123456")
+    //      .option("dbtable", "driver_info")
+    //      .option("driver", "com.mysql.jdbc.Driver")
+    //      .load()
+    //    driver_info.show()
 
 
     // ----------------------获取hbase数据库的order_info表---------------------
@@ -68,20 +72,23 @@ object QueryController {
           GlobalConfigUtils.hbase_table_name -> GlobalConfigUtils.table_order_info,
           GlobalConfigUtils.hbase_table_schema -> GlobalConfigUtils.orderInfo_HbaseSchema
         )).load()
-//    val order_info: DataFrame = readHbase.readHbase_order_info(sparkConf , sparkSession)
-    //创建订单信息表
+    //  测试读取hbase表
+    //  val order_info: DataFrame = readHbase.readHbase_order_info(sparkConf , sparkSession)
+    //  创建订单信息表
     order_info.createOrReplaceTempView("order_info")
 
-    //本地测试读取mysql数据库的order_info表
-//    val order_info: DataFrame = sparkSession.read.format("jdbc")
-//      .option("url", "jdbc:mysql://47.111.68.2:3306/och_test?useUnicode=true&characterEncoding=utf8")
-//      .option("user", "root")
-//      .option("password", "123456")
-//      .option("dbtable", "order_info")
-//      .option("driver", "com.mysql.jdbc.Driver")
-//      .load()
+    //  本地测试读取mysql数据库的order_info表
+    //    val order_info: DataFrame = sparkSession.read.format("jdbc")
+    //      .option("url", "jdbc:mysql://47.111.68.2:3306/och_test?useUnicode=true&characterEncoding=utf8")
+    //      .option("user", "root")
+    //      .option("password", "123456")
+    //      .option("dbtable", "order_info")
+    //      .option("driver", "com.mysql.jdbc.Driver")
+    //      .load()
+    //    order_info.show()
 
-    //-----------------------------读取hbase数据库的司管方信息表-----------------------
+
+    //---------------------------读取hbase数据库的司管方信息表-----------------------
     var opt_alliance_business = sparkSession
       .read
       .format(GlobalConfigUtils.customHbasePath)
@@ -91,22 +98,24 @@ object QueryController {
           GlobalConfigUtils.hbase_table_name -> GlobalConfigUtils.table_alliance_business,
           GlobalConfigUtils.hbase_table_schema -> GlobalConfigUtils.alliance_business_HbaseSchema
         )).load()
-//    val opt_alliance_business = readHbase.readHbase_optAlliance_business(sparkConf , sparkSession)
-    //创建司管方信息表
+    //  测试读取hbase表
+    //  val opt_alliance_business = readHbase.readHbase_optAlliance_business(sparkConf , sparkSession)
+    //  创建司管方信息表
     opt_alliance_business.createOrReplaceTempView("opt_alliance_business")
 
-    //本地测试读取mysql数据库的opt_alliance_business表
-//    val opt_alliance_business: DataFrame = sparkSession.read.format("jdbc")
-//      .option("url", "jdbc:mysql://47.111.68.2:3306/och_test?useUnicode=true&characterEncoding=utf8")
-//      .option("user", "root")
-//      .option("password", "123456")
-//      .option("dbtable", "opt_alliance_business")
-//      .option("driver", "com.mysql.jdbc.Driver")
-//      .load()
+    //  本地测试读取mysql数据库的opt_alliance_business表
+    //    val opt_alliance_business: DataFrame = sparkSession.read.format("jdbc")
+    //      .option("url", "jdbc:mysql://47.111.68.2:3306/och_test?useUnicode=true&characterEncoding=utf8")
+    //      .option("user", "root")
+    //      .option("password", "123456")
+    //      .option("dbtable", "opt_alliance_business")
+    //      .option("driver", "com.mysql.jdbc.Driver")
+    //      .load()
+    //    opt_alliance_business.show()
+
 
     //######################统计司机活动时间###################
     sparkSession.sql(VehicleSQL2.driver_online_record_time_rank).createOrReplaceTempView("driver_online_record_time_rank")
-//    sparkSession.sql(VehicleSQL2.driver_online_record_time_rank).show()
     sparkSession.sql(VehicleSQL2.driver_online_record_time_rank_desc).createOrReplaceTempView("driver_online_record_time_rank_desc")
     sparkSession.sql(VehicleSQL2.driver_online_record_sameday).createOrReplaceTempView("driver_online_record_sameday")
     sparkSession.sql(VehicleSQL2.driver_online_record_yesterday).createOrReplaceTempView("driver_online_record_yesterday")
@@ -121,15 +130,31 @@ object QueryController {
     frame.show()
 
 
+    //    测试数据库
+    //    val url = "jdbc:mysql://120.27.208.185:3306/och_test?useUnicode=true&characterEncoding=utf8"
+    //    val connectionProperties = new Properties()
+    //    connectionProperties.setProperty("user", "och_test"); // 设置用户名
+    //    connectionProperties.setProperty("password", "Kop21IQ8"); // 设置密码
+    //    frame.write.mode(SaveMode.Append).jdbc(url, "driver_online_time", connectionProperties)
 
-    val url = "jdbc:mysql://47.111.68.2:3306/och_test?useUnicode=true&characterEncoding=utf8"
-    val connectionProperties = new Properties()
-    connectionProperties.setProperty("user", "root"); // 设置用户名
-    connectionProperties.setProperty("password", "123456"); // 设置密码
-    connectionProperties.setProperty("driver", "com.mysql.jdbc.Driver")
-    frame.write.mode(SaveMode.Append).jdbc(url, "driver_online_time_copy", connectionProperties)
+
+    //    高质数据库
+    //    val url = "jdbc:mysql://47.111.68.2:3306/och_test?useUnicode=true&characterEncoding=utf8"
+    //    val connectionProperties = new Properties()
+    //    connectionProperties.setProperty("user", "root"); // 设置用户名
+    //    connectionProperties.setProperty("password", "123456"); // 设置密码
+    //    connectionProperties.setProperty("driver", "com.mysql.jdbc.Driver")
+    //    frame.write.mode(SaveMode.Append).jdbc(url, "driver_online_time_copy", connectionProperties)
 
 
+    //    开发mysql数据库（未提供）
+    //    val url = "jdbc:mysql://120.27.208.185:3306/och_test?useUnicode=true&characterEncoding=utf8"
+    //    val connectionProperties = new Properties()
+    //    connectionProperties.setProperty("user", "och_test"); // 设置用户名
+    //    connectionProperties.setProperty("password", "Kop21IQ8"); // 设置密码
+    //    frame.write.mode(SaveMode.Append).jdbc(url, "driver_online_time", connectionProperties)
+
+    //  释放资源
     sparkSession.stop()
     System.exit(0)
 
